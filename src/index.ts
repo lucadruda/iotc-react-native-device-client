9c02168255900b1ad0aa81e9f971fd58373e6b9c
lucadruda <ludruda@microsoft.com>
2020-09-28 19:51:57 +0200

    export error

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import IoTCClient from './client';
 import ProvisioningClient, { HubCredentials } from './provision';
-import { IOTC_CONNECT, IOTC_EVENTS, IOTC_LOGGING } from './types/constants';
+import { IOTC_CONNECT, IOTC_EVENTS, IOTC_LOGGING, CancellationException } from './types/constants';
 
 
-export { IoTCClient, ProvisioningClient, HubCredentials, IOTC_CONNECT, IOTC_EVENTS, IOTC_LOGGING };
+export { IoTCClient, ProvisioningClient, HubCredentials, IOTC_CONNECT, IOTC_EVENTS, IOTC_LOGGING, CancellationException };
 export * from './types/interfaces';
 export * from './utils';
