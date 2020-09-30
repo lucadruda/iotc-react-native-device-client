e193f4ce78fa7bbe351c29386b78cabd61359ab0
lucadruda <ludruda@microsoft.com>
2020-09-30 12:42:58 +0200

    fix default export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import IoTCClient from './client';
 import ProvisioningClient, { HubCredentials } from './provision';
 import { IOTC_CONNECT, IOTC_EVENTS, IOTC_LOGGING, CancellationException } from './types/constants';
 
-export { default } from './cancellationToken';
+export { default as CancellationToken } from './cancellationToken';
 export { IoTCClient, ProvisioningClient, HubCredentials, IOTC_CONNECT, IOTC_EVENTS, IOTC_LOGGING, CancellationException };
 export * from './types/interfaces';
 export * from './utils';
